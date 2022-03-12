c4df4226001450f00bd2961bae1b53cda2f832fa
luis <luis.musica100@gmail.com>
2022-03-11 22:55:08 -0600

    version 1

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server =http.createServer(app)
+
+const {Server} = require('socket.io')
+const io= new Server(server)
+
+io.on('connection',(socket)=>{
+    //console.log('un usuario se ha conectado')
+   /* socket.on('chat',(msg)=>{
+        console.log('Mensaje: '+msg)
+    })*/
+    socket.on('chat',(msg)=>{
+        io.emit('chat',msg)
+    })
+})
+app.get('/',(req,res)=>{
+    //res.send('<h1>aplicacion</h1>')
+    //console.log(__dirname)
+    res.sendFile(`${__dirname}/cliente/index.html`)
+})
+
+//app.listen()
+server.listen(3000, ()=>{
+    console.log('servidor corriendo en http://localhost:3000')
+}) 
